553fba2e4a5121592f7c8e211d26170371685e5f
Im-perfect <zhangke.kerri@gmail.com>
2019-10-19 12:45:45 +0200

    inserted 3 rows of data

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -1,12 +1,34 @@
-const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://postgres:secret@localhost:5432/postgres");
 
 const Movie = db.define("movies", {
-    title: Sequelize.STRING,
-    yearOfRelease: Sequelize.INTEGER,
-    synopsis: Sequelize.STRING
-  });
+  title: Sequelize.STRING,
+  yearOfRelease: Sequelize.INTEGER,
+  synopsis: Sequelize.STRING
+});
 
 db.sync()
-.then(res => console.log('database connected'))
-.catch(console.error)
+  .then(res => console.log("database connected"))
+  .then(() => {
+    Promise.all([
+      Movie.create({
+        title: "Joker",
+        yearOfRelease: 2019,
+        synopsis:
+          'In Gotham City, mentally-troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: "The Joker".'
+      }),
+      Movie.create({
+        title: "El Camino: A Breaking Bad Movie",
+        yearOfRelease: 2019,
+        synopsis:
+          "A sequel, of sorts, to Breaking Bad following Jesse Pinkman after the events captured in the finale of Breaking Bad. Jesse is now on the run, as a massive police manhunt for him is in operation."
+      }),
+      Movie.create({
+        title: "Gemini Man",
+        yearOfRelease: 2019,
+        synopsis:
+          "An over-the-hill hitman faces off against a younger clone of himself."
+      })
+    ]);
+  })
+  .catch(console.error);

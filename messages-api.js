ec37a35bb520e8d83261911e41af59e971544da2
Im-perfect <zhangke.kerri@gmail.com>
2019-10-19 12:24:28 +0200

    add another version of limit middleware, not sure what the requirement means

--- messages-api.js
+++ messages-api.js
@@ -8,7 +8,8 @@ const bodyParser = require("body-parser");
 const bodyParserMiddleware = bodyParser.json();
 app.use(bodyParserMiddleware);
 
-//Limit middleware
+//Limit middleware --> only allow 5 requests, including BAD and good requestes
+/*
 const limit = 5;
 let reqLeft = limit
 const sendLimitMiddleware = (req, res, next) => {
@@ -25,6 +26,31 @@ app.post("/messages", sendLimitMiddleware, (req, res, next) => {
     res.status(400).end();
   }
 
+  console.log("TEXT received: ", req.body.text);
+  res.json({
+    message: "Message received loud and clear"
+  });
+});
+*/
+
+
+//Limit middleware --> allow 5 good requests, ignore bad requests
+const limit = 5;
+let reqLeft = limit;
+const sendLimitMiddleware = (req, res, next) => {
+  if (!req.body.text) {
+    res.status(400).end();
+  } else {
+    if (!reqLeft) {
+      res.status(429).end();
+    } else {
+      reqLeft -= 1;
+      next();
+    }
+  }
+};
+
+app.post("/messages", sendLimitMiddleware, (req, res, next) => {
   console.log("TEXT received: ", req.body.text);
   res.json({
     message: "Message received loud and clear"
